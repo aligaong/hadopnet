7c0dd78815fa9cfd8d3cb8462bd21af58ed9aa1b
aligaong <133207298+aligaong@users.noreply.github.com>
2023-05-11 17:14:25 +0800

    Update AppTest.java

--- hdfs/src/test/java/com/firecode/hadooptest/hdfs/AppTest.java
+++ hdfs/src/test/java/com/firecode/hadooptest/hdfs/AppTest.java
@@ -14,11 +14,7 @@ public class AppTest
      * Create the test case
      *
      * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+
 
     /**
      * @return the suite of tests being tested
